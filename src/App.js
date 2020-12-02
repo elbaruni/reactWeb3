15b19b07960f167fab4118656b864a4ceaec08bf
bluepine <bluepine@bluepine.com>
2020-12-02 11:45:50 +0500

    Update the script according to the logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
- 
 import Web3 from 'web3';
-
-
 import React, { useState,useEffect } from 'react';
-import rvc from "./contracts/RVC.json"
- 
+import rcvr from "./contracts/RCVR.json";
+import migration from "./contracts/Migration.json"
 
 const networks={
   "1":"MainNet",
@@ -16,63 +13,73 @@ const networks={
 
 const App = props => {
   const [account,setAccount]=useState("")
- 
   const [balance,setBalance]=useState(0)
-  const [networkId,setNetworkId]=useState( "")
-   
-
- //Function to check if account changed and set it to account state 
- const getRVCBalance=async ()=>{
-
-  try {  
-    if(networkId=="3"){
-      const rvcContract=await new window.web3.eth.Contract(rvc.abi, rvc.address) 
-      const balance=await rvcContract.methods.balanceOf(account).call()
-    setBalance(window.web3.utils.fromWei( balance,'ether') )  
-    }else{
-      console.log("wrong network")
-    }
-   }
-  catch(e){
-    console.log(e.message)
-  }
- 
-   
-   
- }
- const writeFunction=async()=>{
-
-
-  try {  
-    if(networkId=="3"){
-      
-    
-      
-      const user="0xf26D566aF179f5499108C459ef41725F370447c9"
-      const amount= 10*10**6//new BN(1234) ; 
-      const rvcContract=await new window.web3.eth.Contract(rvc.abi, rvc.address) 
-      const approvel=await rvcContract.methods.approve(user,amount).send({from:account})
-    
-      
-     console.log(approvel)
-    }else{
-      console.log("wrong network")
-    }
-   }
-  catch(e){
-    console.log(e.message)
-  }
+  const [networkId,setNetworkId]=useState("")
+  const [stake,setStake]=useState("")
   
- 
+  //Function to check if account changed and set it to account state 
+ const getRCVRBalance=async ()=>{
+
+  try {  
+    if(networkId=="3"){
+      const rcvrContract=await new window.web3.eth.Contract(rcvr.abi, rcvr.address) 
+      const balance=await rcvrContract.methods.balanceOf(account).call()
+      setBalance(window.web3.utils.fromWei( balance,'ether') )  
+    } else {
+      console.log("wrong network")
+      setBalance(null)  
+    }
+  }
+  catch(e){
+    console.log(e.message)
+  }
  }
- 
- const writeFunction2=async()=>{
+
+ const createSafeStake=async()=>{
+  try {  
+    if(networkId=="3"){
+      const web3= new Web3;
+      const BN = web3.utils.BN;
+      let amount = parseInt(stake);
+
+      const rcvrContract=await new window.web3.eth.Contract(rcvr.abi, rcvr.address) 
+      const txHash = await rcvrContract.methods.createSafeStake(web3.utils.toWei(`${amount}`, 'ether')).send({from:account})
+      console.log(txHash.transactionHash);
+    } else {
+      console.log("wrong network")
+    }
+   }
+  catch(e){
+    console.log(e.message)
+  }
+ }
+
+ const removeStake =async()=>{
+  try {  
+    if(networkId=="3"){  
+      const web3= new Web3;
+      const BN = web3.utils.BN;
+      let amount = parseInt(stake);
+
+      const rcvrContract=await new window.web3.eth.Contract(rcvr.abi, rcvr.address) 
+      const txHash=await rcvrContract.methods.removeStake(web3.utils.toWei(`${amount}`, 'ether')).send({from:account})
+      console.log(txHash.transactionHash)
+    } else {
+      console.log("wrong network")
+    }
+   }
+  catch(e){
+    console.log(e.message)
+  }
+ }
+
+ const forceDistribute1=async()=>{
   try {  
     if(networkId=="3"){
       const web3= new Web3
-     
-      const rvcContract=await new window.web3.eth.Contract(rvc.abi, rvc.address) 
-      const txHash=await rvcContract.methods.Forcedistribute(true).send({from:account,value:web3.utils.toWei('0.1','ether')})
+     debugger
+      const rcvrContract=await new window.web3.eth.Contract(rcvr.abi, rcvr.address) 
+      const txHash=await rcvrContract.methods.Forcedistribute(true).send({from:account,value:web3.utils.toWei('0.1','ether')})
      
      console.log(txHash.transactionHash)
     }else{
@@ -82,41 +89,68 @@ const App = props => {
   catch(e){
     console.log(e.message)
   }
+ }
+
+ const forceDistribute2=async()=>{
+  try {  
+    if(networkId=="3"){
+      const web3= new Web3
+     
+      const rcvrContract=await new window.web3.eth.Contract(rcvr.abi, rcvr.address) 
+      const txHash=await rcvrContract.methods.Forcedistribute(false).send({from:account,value:web3.utils.toWei('0.1','ether')})
+     
+     console.log(txHash.transactionHash)
+    }else{
+      console.log("wrong network")
+    }
+   }
+  catch(e){
+    console.log(e.message)
+  }
+ }
+ const migrate=async()=>{
+  try {  
+    if(networkId=="3"){
+      const web3= new Web3
+     
+      const migrationContract=await new window.web3.eth.Contract(migration.abi, migration.address) 
+      const txHash=await migrationContract.methods.migrate().send({from:account})
+     
+     console.log(txHash.transactionHash)
+    }else{
+      console.log("wrong network")
+    }
+   }
+  catch(e){
+    console.log(e.message)
+  }
+ }
 
  
-
- }
  const getAccount=async ()=>{
-
   const web3 = window.web3
   // Load account
   const accounts = await web3.eth.getAccounts()
   console.log(accounts)
   setAccount(accounts[0])
-  
-   
  }
   useEffect(() => {
-   setNetworkId(window.ethereum.networkVersion)
+    setNetworkId(window.ethereum.networkVersion)
     window.ethereum.on('networkChanged', function(networkId){
       console.log('networkChanged',networkId);
-      setNetworkId(networkId)
+      setNetworkId(networkId);
+      getRCVRBalance(); 
     });
     window.ethereum.on('accountsChanged', function (accounts) {
-      setAccount(accounts[0])
-    
-})  
-   
-      
-    
+      setAccount(accounts[0]);
+      getRCVRBalance();
+    });
   } ,[]);
  
 const  connectWeb3=async()=>{
   if (window.ethereum) {
     window.web3 = new Web3(window.ethereum)
     await window.ethereum.enable()
-   
-    //getRVCBalance()  
   }
   else if (window.web3) {
     window.web3 = new Web3(window.web3.currentProvider)
@@ -125,22 +159,35 @@ const  connectWeb3=async()=>{
     window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
   }
   getAccount()  
-  await getRVCBalance()
+  await getRCVRBalance()
 }
   let content = (
     <React.Fragment>
-      <p>{account}</p>
-      <p>{balance}</p>
-      <p>{ networks[networkId]}</p>
-      <button id="test" onClick={writeFunction}>test</button>
-      <button id="test1" onClick={writeFunction2}>test2</button>
-      <button onClick={connectWeb3}>connect</button>
-       
+      <div  style={{padding:'30px'}}>
+        <p>Address: {account}</p>
+        <p>RCVR Balance: {balance}</p>
+        <p>NetworkId: { networks[networkId]}</p>
+        <p>         
+          <button onClick={connectWeb3}>connect</button>
+        </p>
+        <hr/>
+        <p>
+          <input type="number" min="1" max="1000000" defaultValue={stake} onChange={(e) => setStake(e.target.value)}/>
+          <br/>
+          <button id="btn1" onClick={createSafeStake}>Create Stake</button>
+          <button id="btn2" onClick={removeStake}>Remove Stake</button>
+        </p>
+        <p>
+          <button id="btn3" onClick={forceDistribute1}>Force Calculation 1</button>
+          <button id="btn4" onClick={forceDistribute2}>Force Calculation 2</button>
+        </p>
+        <hr/>
+        <p>
+          <button id="btn5" onClick={migrate}>Migrate</button>
+        </p>
+       </div>
     </React.Fragment>
   );
-
- 
   return content;
 };
-
 export default App;
